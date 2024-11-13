01e8c2345f2e8cac87c686bef3023fa4914121dd
BLipsky <BenLipsky@icloud.com>
2024-11-13 10:07:40 -0500

    Add timer function for auto-advancing slides

--- gallery.js
+++ gallery.js
@@ -1,22 +1,19 @@
 let mCurrentIndex = 0; // Tracks the current image index
-let mImages = []; // Array to hold GalleryImage objects
+let mImages = []; // Tracks the current image index
 const mUrl = "images.json"; // Replace with actual JSON URL
 const mWaitTime = 5000; // Timer interval in milliseconds
 
 $(document).ready(() => {
-  $(".details");
-  // Call a function here to start the timer for the slideshow
-
-  // Select the moreIndicator button and add a click event to:
-  // - toggle the rotation classes (rot90 and rot270)
-  // - slideToggle the visibility of the .details section
-
-  // Select the "Next Photo" button and add a click event to call showNextPhoto
-
-  // Select the "Previous Photo" button and add a click event to call showPrevPhoto
-  // Call fetchJSON() to load the initial set of images
+  $(".details").hide();
+  startTimer();
   fetchJSON();
+
+  $(".moreIndicator").on("click", function() {
+    $(this).toggleClass("rot90 rot270");
+    $(".details").slideToggle();
+  });
 });
+
 // Function to fetch JSON data and store it in mImages
 function fetchJSON() {
   $.ajax({
@@ -28,37 +25,41 @@ function fetchJSON() {
     },
   });
 }
+
 // Function to swap and display the next photo in the slideshow
 function swapPhoto() {
   let image = mImages[mCurrentIndex];
   $("#photo").attr("src", image.imgPath);
-  $(".location").html(image.imgLocation);
-  $(".description").html(image.description);
-  $(".state").html(image.state);
+  $(".location").html("Location: " + image.imgLocation);
+  $(".description").html("Description: " + image.description);
+  $(".state").html("State: " + image.state);
 }
 
 // Advances to the next photo, loops to the first photo if the end of array is reached
-$("#nextPhoto").on("click", function showNextPhoto() {
+function showNextPhoto() {
   mCurrentIndex++;
-  if (mCurrentIndex > mImages.length) {
+  if (mCurrentIndex >= mImages.length) {
     mCurrentIndex = 0;
   }
   swapPhoto();
-});
+}
 
 // Goes to the previous photo, loops to the last photo if mCurrentIndex goes negative
-$("#prevPhoto").on("click", function showPrevPhoto() {
+function showPrevPhoto() {
   mCurrentIndex--;
   if (mCurrentIndex < 0) {
-    mCurrentIndex = 13;
+    mCurrentIndex = mImages.length - 1;
   }
   swapPhoto();
-});
-
+}
+$("#nextPhoto").on("click", showNextPhoto);
+$("#prevPhoto").on("click", showPrevPhoto);
 
 // Starter code for the timer function
 function startTimer() {
-  // Create a timer to automatically call `showNextPhoto()` every mWaitTime milliseconds
-  // Consider using setInterval to achieve this functionality
-  // Hint: Make sure only one timer runs at a time
+  let timer = setInterval(showNextPhoto, mWaitTime);
+  $(".moreIndicator").on("click", function() {
+    clearInterval(timer);
+    timer = setInterval(showNextPhoto, mWaitTime);
+  });
 }
